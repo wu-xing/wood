5ac8ef6240ec56498b2fafeab204ba594a2ce1a8
fwchen <me@chenfangwei.com>
2018-06-21 15:45:55 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ import 'element-ui/lib/theme-chalk/index.css';
 
 import { setupAxiosAuth } from './util/axios'
 import store from './store'
+
 setupAxiosAuth();
 
 Vue.config.productionTip = true;
--- src/router.ts
+++ src/router.ts
@@ -11,6 +11,7 @@ const SignUp = async () => await require('./views/SignUp.vue');
 const Articles = async () => await require('./views/Articles.vue');
 const ArticleEdit = async () => await require('./views/ArticleEdit.vue');
 const ArticleCreate = async () => await require('./views/ArticleCreate.vue');
+const Unauthorized = async () => await require('./views/Unauthorized.vue');
 
 export default new Router({
   mode: 'history',
@@ -46,8 +47,8 @@ export default new Router({
     },
     {
       path: '/401',
-      name: 'home',
-      component: Home
+      name: '401',
+      component: Unauthorized
     },
     {
       path: '/about',
