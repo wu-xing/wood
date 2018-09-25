698b964bfb6a9c040fa4a9020057737a10079bc1
fwchen <me@chenfangwei.com>
2018-09-25 11:43:23 +0800

    add share article path

--- src/router.ts
+++ src/router.ts
@@ -12,6 +12,7 @@ const Articles = async () => await require('./views/Articles.vue');
 const ArticleEdit = async () => await require('./views/ArticleEdit.vue');
 const ArticleCreate = async () => await require('./views/ArticleCreate.vue');
 const Unauthorized = async () => await require('./views/Unauthorized.vue');
+const ShareArticle = async () => await require('./views/shareArticle.vue');
 
 export default new Router({
   mode: 'history',
@@ -40,6 +41,11 @@ export default new Router({
       path: '/article/:id',
       name: 'article',
       component: ArticleEdit
+    },
+    {
+      path: '/p/:id',
+      name: 'share-article',
+      component: ShareArticle
     },
     {
       path: '/',
---
+++ src/views/ShareArticle.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>401</h1>
+
+    <a href="/signin">signin</a>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import axios from 'axios';
+
+@Component({})
+export default class ShareArticle extends Vue {
+
+  getArticle() {
+    const aritcleId = this.$route.params.id;
+    axios.get(`/api/article/${aritcleId}`).then(resp => {
+      const article = resp.data;
+    });
+  }
+
+  created() {
+    this.getArticle();
+  }
+}
+</script>
