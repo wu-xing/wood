4088111851f809371095577a59548b81e31ecb9d
fwchen <me@chenfangwei.com>
2018-10-15 19:50:42 +0800

    initial ArticleOperation component

---
+++ src/components/ArticleOperation.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class ArticleOperation extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<style scoped>
+</style>
