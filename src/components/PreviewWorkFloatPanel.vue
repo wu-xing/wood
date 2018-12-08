7e2e13a7269ab28d16daf134cef88da3196427a6
fwchen <me@chenfangwei.com>
2018-12-08 14:34:06 +0800

    initial float info panel component

---
+++ src/components/PreviewWorkFloatPanel.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div>
+      <as-icon name="poll-h"></as-icon>
+    </div>
+    <div>
+
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class PreviewWorkFloatPanel extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
