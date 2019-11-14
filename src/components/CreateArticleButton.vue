f10bc0bc0dd96c6868c2452df83e1abd96a7f1c6
fwchen <me@chenfangwei.com>
2019-11-14 22:37:34 +0800

    update

--- src/components/CreateArticleButton.vue
+++ src/components/CreateArticleButton.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <el-button v-on:click="innerOnClick">新建笔记</el-button>
+    <el-button  icon="el-icon-plus" type="primary" size="small" v-on:click="innerOnClick" round>新建笔记</el-button>
 
     <el-dialog
       title="选择笔记类型"
@@ -8,18 +8,37 @@
       :visible.sync="dialogVisible"
       :before-close="handleClose"
     >
-      hi    
+      <SelectList v-bind:options="options" v-on:select="onSelected()"></SelectList>
     </el-dialog>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import { SelectListOption } from '../typing/widget';
+import SelectList from './SelectList.vue';
+import axios from 'axios';
 
-@Component
+@Component({
+  components: {
+    SelectList
+  }
+})
 export default class CreateArticleButton extends Vue {
-  //   @Prop()
-  //   public onClick!: Function;
+  public options: SelectListOption[] = [
+    {
+      label: 'Markdown',
+      value: 'Markdown'
+    },
+    {
+      label: 'Org-mode',
+      value: 'Org-mode'
+    },
+    {
+      label: '富文本',
+      value: 'Rich'
+    }
+  ];
 
   public dialogVisible = false;
 
@@ -29,6 +48,38 @@ export default class CreateArticleButton extends Vue {
 
   innerOnClick() {
     this.dialogVisible = true;
+  }
+
+  onSelected(option: SelectListOption) {
+    const document = {
+      content: '',
+      title: '',
+      createdAt: new Date().getTime(),
+      type: option.value
+    };
+    axios
+      .post('/api/note', document)
+      .then(resp => {
+        this.$store.commit('article', {
+          id: resp.data.id,
+          ...document
+        });
+        this.saved = false;
+        window.localStorage.removeItem(DRAFT_KEY);
+        router.push({
+          name: 'article-edit',
+          params: {
+            id: resp.data.id
+          }
+        });
+      })
+      .catch(error => {
+        Message({
+          message: 'Add article failure.',
+          type: 'error'
+        });
+        this.saved = false;
+      });
   }
 }
 </script>
--- src/components/SelectList.vue
+++ src/components/SelectList.vue
@@ -1,20 +1,36 @@
 <template>
   <ul>
-      <li v-for=""></li>
+    <li
+      v-for="option of options"
+      v-bind:key="option.value"
+      v-on:click="onClick(option)"
+    >{{option.label}}</li>
   </ul>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import { SelectListOption } from '../typing/widget';
 
 @Component
 export default class SelectList extends Vue {
- @Prop() options: any[];
+  @Prop() options: SelectListOption[];
 
- 
+  onClick(option: SelectListOption) {
+    this.$emit('select', option);
+  }
 }
 </script>
 
 <style scoped>
+ul {
+  list-style: none;
+}
 
+
+
+
+li {
+  height: 50px;
+}
 </style>
